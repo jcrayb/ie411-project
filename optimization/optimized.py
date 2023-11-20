f3f831eb0618db2cf888a41d2494ff5350f6f8f7
jcrayb <jcrb7800@gmail.com>
2023-11-20 13:53:59 -0600

    Illustration

--- optimization/optimized.py
+++ optimization/optimized.py
@@ -4,94 +4,63 @@ from gurobipy import GRB
 import numpy as np
 import networkx as nx
 import time
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
 
 im = Image.open('tower.png').convert('L') #convert to grayscale
-
-im.save('new_img.png')
 tic = time.time()
 
-'''h = 5
-w= 5'''
-
 s = 0 
-
 real_source = 50
-#grayscale values; list of size h*w , where each values is integer in range [0,255]
-
 iterations = 25
-
 
 #%%
 def idx(i,j):
     return w*(i-1) + j
 
-for z in range(iterations):
-    def get_weight(n1,n2): #weight of edge connecting n1 and n2 
-        if n1== s :
-            diff = 0
-        elif n2 == t:
-            diff = 0
-        else:
-            diff = np.abs(intensity[n1-1] - intensity[n2-1]) 
-        return diff
+def get_weight(n1,n2): #weight of edge connecting n1 and n2 
+    if n1== s or n2 == t:
+        return 0
+    else:
+        return np.abs(intensity[n1-1] - intensity[n2-1]) 
 
-    im = Image.open('new_img.png').convert('L')
+for z in range(iterations):
     h = im.height
     w = im.width
     t = h*w+1
     intensity = list(im.getdata())
-    coords = [(i,j) for i in range(1,h+1) for j in range(1,w+1)]
+
+    coords = [(i,j) for i in range(1,h+1) for j in range(1,w+1) if (real_source-i<=j<=100)]
 
     nodes = [0]
     nodes += [idx(i, j) for i, j in coords]
 
     nodes += [h*w+1]
-    #print(nodes)
-
-
-
-
 
     ngbrs = {idx(i,j) :[idx(i2,j2) for (i2,j2) in [(i+1,j-1),(i+1,j),(i+1,j+1)]
-            if 1 <= i2 <= h and 1 <= j2 <= w]
+            if 1 <= i2 <= h and max(14, real_source-i2)<= j2 <= min(100, real_source+i2)]
             for (i,j) in coords}
-
-
 
     ngbrs[0] = [real_source]
 
-    for j in range(1, w+1):
+    for j in range(14, 100-z):
         ngbrs[idx(h, j)].append(h*w+1)
 
 
     edges = [(n1,n2) for n1 in nodes[:-1] for n2 in ngbrs[n1]]
-    #print(nodes)
-    #print(edges)
 
     G = nx.DiGraph()
     G.add_nodes_from(nodes)
     G.add_edges_from(edges)
 
     A = nx.incidence_matrix(G, oriented=True)
-    #print(A)
     b = np.zeros(G.number_of_nodes())
 
     b[0] = -1
     b[-1] = 1
-
-    #print(b)
-
+    
     r = np.zeros(G.number_of_edges())
 
     for i in range(G.number_of_edges()):
-        
         r[i] = get_weight(edges[i][0],edges[i][1])
-        #print(edges[i], r[i])
-
-    #print(r)
 
     m = gp.Model("lp")
     m.Params.LogToConsole = 0
@@ -102,27 +71,20 @@ for z in range(iterations):
     obj = r@f
     m.setObjective(obj, GRB.MINIMIZE)
     m.optimize()
-    flows = m.getAttr("X", m.getVars())
-    print("Primal objective: ", m.getObjective().getValue())
-    #print(flows)
-    '''for f in flows:
-        if f>0:
-            print('woohoo')'''
 
+    flows = m.getAttr("X", m.getVars())[1:]
     img = np.array(im)
-    
-    flowing = []
+
     new_img = []
-    flows = flows[1:]
     for i in range(len(flows)):
         if flows[i]==1:
             endpoint = edges[i][0]
-            coord = (endpoint//w, endpoint%w)
-            flowing += [coord]
-            #print(coord)
-            new_img += [np.delete(img[coord[0]], coord[1]-1)]
+            new_img += [np.delete(img[endpoint//w], endpoint%w-1)]
 
-    print(z+1)
+    im = Image.fromarray(np.array(new_img))
+    print(f"Iteration {z+1}")
+
 toc = time.time()
 print ('elapsed ', toc - tic)
+im.save('saved.png')
  
